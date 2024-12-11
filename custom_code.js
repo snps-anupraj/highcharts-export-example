dad6bfe044465c8790dc1f8edc32579b2dbd3f7a
Anup Raj <anupraj@synopsys.com>
2024-12-11 11:02:30 +0530

    Chanes Suggested from Highcharts Support

--- custom_code.js
+++ custom_code.js
@@ -1,36 +1,42 @@
 Highcharts.setOptions({
   chart: {
     events: {
-      load: function () {
+      render: function () {
         const chart = this,
           xAxis = chart.xAxis[0],
           boxplotSeries = chart.series.filter(
-            (series) => series.options.type === "boxplot",
+            (series) => series.options.type === 'boxplot'
           ),
           scatterSeries = chart.series.filter(
-            (series) => series.options.type === "scatter",
-          )
+            (series) => series.options.type === 'scatter'
+          );
+
+        if (chart.redrawnAlready) {
+          return;
+        }
 
         boxplotSeries.forEach((boxplot) => {
           const scatter = scatterSeries.filter(
-            (series) => series.options.linkedTo === boxplot.options.id,
-          )
-          const boxplotPoint = boxplot.points.find((point) => !point.isNull)
+            (series) => series.options.linkedTo === boxplot.options.id
+          );
+          const boxplotPoint = boxplot.points.find((point) => !point.isNull);
           if (boxplotPoint && boxplotPoint.graphic) {
-            const boxplotBox = boxplotPoint.graphic.getBBox()
-            console.log(boxplotBox.x)
+            const boxplotBox = boxplotPoint.graphic.getBBox();
+            console.log(boxplotBox.x);
             scatter.forEach((series) => {
               series.update(
                 {
                   pointPlacement: xAxis.toValue(
-                    boxplotBox.x + boxplotBox.width / 2 + chart.plotLeft,
-                  ),
+                    boxplotBox.x + boxplotBox.width / 2 + chart.plotLeft
+                  )
                 },
-                false,
-              )
-            })
+                false
+              );
+            });
           }
         });
+
+        chart.redrawnAlready = true;
         chart.redraw();
       }
     }
